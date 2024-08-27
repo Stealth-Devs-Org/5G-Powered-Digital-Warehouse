19341a7294bc0d84973a28a7fa3997c32175e9a2
SAIRISAN123 <sairisan123@gmail.com.com>
2024-08-27 12:14:46 +0530

    added weather object

--- Assets/Scripts/WeatherForecast/weatherController.cs
+++ Assets/Scripts/WeatherForecast/weatherController.cs
@@ -1,28 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class WeatherController : MonoBehaviour {
- [SerializeField] Material sky;
- [SerializeField] Light sun;
- private float fullIntensity;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
 
- public string cityName = "Kalmunai,LK";
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] GameObject GlobalVolume; // Reference to the Global Volume in the scene
+    [SerializeField] Material skyboxManual; // Reference to the skybox material
 
- void OnEnable() {
- Messenger.AddListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated);
- }
- void OnDisable() {
- Messenger.RemoveListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated);
- }
- void Start() {
- fullIntensity = sun.intensity;
- }
+    private float fullIntensity; // Stores the initial intensity for lights
+    private Exposure exposureComponent; // Reference to the Exposure component in Global Volume
 
- private void OnWeatherUpdated() {
- SetOvercast(Managers.Weather.cloudValue);
- }
- private void SetOvercast(float value) {
- sky.SetFloat("_Blend", value);
- sun.intensity = fullIntensity - (fullIntensity * value);
- }
+    public string cityName = "Kalmunai,LK"; // City name for weather data
+
+    void OnEnable()
+    {
+        Messenger.AddListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated); // Subscribes to weather updates
+    }
+
+    void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated); // Unsubscribes from weather updates
+    }
+
+    void Start()
+    {
+        // Get the Exposure component from the Global Volume
+        if (GlobalVolume.TryGetComponent<Volume>(out Volume volume))
+        {
+            if (volume.profile.TryGet<Exposure>(out exposureComponent))
+            {
+                fullIntensity = exposureComponent.limitMin.value; // Store the initial limit min value
+            }
+        }
+    }
+
+    private void OnWeatherUpdated()
+    {
+        // Update the overcast settings based on the weather manager's cloud value
+        SetOvercast(Managers.Weather.cloudValue);
+    }
+
+    private void SetOvercast(float value)
+    {
+        if (exposureComponent != null)
+        {
+            // Adjust the limit min value based on the cloud cover
+            float adjustedIntensity = fullIntensity - (fullIntensity * value);
+            exposureComponent.limitMin.value = adjustedIntensity;
+        }
+    }
 }
