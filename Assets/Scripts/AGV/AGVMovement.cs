429695506812dfe178238518825b8d5057de269e
Rajaretnam Sairisan <sairisan123@gmail.com>
2024-09-24 11:24:53 +0530

    Remove unnecessary files and folders

---
+++ Assets/Scripts/AGV/AGVMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AGVMovement : MonoBehaviour
+{
+    public float speedofAGV =5.0f;
+    public float rotationSpeed = 0.5f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speedofAGV);        
+    }
+}
