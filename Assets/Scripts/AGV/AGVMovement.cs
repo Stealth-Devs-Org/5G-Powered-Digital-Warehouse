0d8ae8823d227ce4ce1923288ace7d9c807b813f
Rajaretnam Sairisan <sairisan123@gmail.com>
2024-09-28 15:47:15 +0530

    AGVMovement Commented Need to be done

--- Assets/Scripts/AGV/AGVMovement.cs
+++ Assets/Scripts/AGV/AGVMovement.cs
@@ -1,27 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class AGVMovement : MonoBehaviour
-{
-    public float speedofAGV =5.0f;
-    public float rotationSpeed = 0.5f;
+// public class AGVMovement : MonoBehaviour
+// {
+//     AGVController AGVController;
+//     public float speedofAGV =5.0f;
+//     public float rotationSpeed = 0.5f;
 
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         AGVController = GetComponent<AGVController>();
+//     }
 
-        
-    }
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         //gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speedofAGV);  
+//         gameObject.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);      
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speedofAGV);  
-        gameObject.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);      
-    }
-}
+
+//     public void MoveAGV(Vector3 targetPosition)
+//     {
+
+//         Vector3 direction = targetPosition - transform.position;
+//         float angle = Vector3.Angle(direction, transform.forward);
+//         float distance = direction.magnitude;
+
+//         if (angle > 1.0f)
+//         {
+//             Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, rotationSpeed * Time.deltaTime, 0.0f);
+//             transform.rotation = Quaternion.LookRotation(newDirection);
+//         }
+//         else
+//         {
+//             transform.position = Vector3.MoveTowards(transform.position, targetPosition, speedofAGV * Time.deltaTime);
+//         }
+//     }
+// }
