38e0cd031d7214ebbf26a5fe0e44bdbfa7a66487
Rajaretnam Sairisan <sairisan123@gmail.com>
2024-09-29 19:50:54 +0530

    Refactor AGVMovement.cs to add support for multiple AGVs and improve movement logic

--- Assets/Scripts/AGV/AGVMovement.cs
+++ Assets/Scripts/AGV/AGVMovement.cs
@@ -1,46 +1,72 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class AGVMovement : MonoBehaviour
-// {
-//     AGVController AGVController;
-//     public float speedofAGV =5.0f;
-//     public float rotationSpeed = 0.5f;
+public class AGVMovement : MonoBehaviour
+{
+    AGVController AGVController;
+    public int numberOfAGV = 4;
+    public float speedofAGV =5.0f;
+    public float rotationSpeed = 0.5f;
+    private bool[] AGVPresent;
 
-
-
-
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         AGVController = GetComponent<AGVController>();
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         //gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speedofAGV);  
-//         gameObject.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);      
-//     }
+    
 
 
-//     public void MoveAGV(Vector3 targetPosition)
-//     {
 
-//         Vector3 direction = targetPosition - transform.position;
-//         float angle = Vector3.Angle(direction, transform.forward);
-//         float distance = direction.magnitude;
 
-//         if (angle > 1.0f)
-//         {
-//             Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, rotationSpeed * Time.deltaTime, 0.0f);
-//             transform.rotation = Quaternion.LookRotation(newDirection);
-//         }
-//         else
-//         {
-//             transform.position = Vector3.MoveTowards(transform.position, targetPosition, speedofAGV * Time.deltaTime);
-//         }
-//     }
-// }
+    // Start is called before the first frame update
+    void Start()
+    {
+        AGVPresent = new bool[numberOfAGV];
+        for (int i = 0; i < numberOfAGV; i++)
+        {
+            AGVPresent[i] = false;  //initially no AGV present...
+        }
+
+        AGVController = GetComponent<AGVController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // for (int i = 0; i < AGVPresent.Length; i++)
+        // {
+        //     if (AGVPresent[i] ==true)
+        //     {
+        //         MoveAGV(AGVController.targetPosition[i]);
+        //     }
+        //     else
+        //     {
+
+        //         AGVPresent[i] = false;
+
+        //     }
+        // }
+        //gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speedofAGV);  
+        gameObject.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);      
+    }
+
+
+
+
+    public void MoveAGV(Vector3 targetPosition)
+    {
+
+        Vector3 direction = targetPosition - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+        float distance = direction.magnitude;
+
+        if (angle > 1.0f)
+        {
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, rotationSpeed * Time.deltaTime, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speedofAGV * Time.deltaTime);
+        }
+    }
+}
